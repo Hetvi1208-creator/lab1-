eefecfcd3c9e02e442597858aef8f6bc9e4c6575
Hetvi1208-creator <hetvibodat1208@gmail.com>
2025-08-07 16:04:12 +0530

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+
+    int a,b,c;
+
+    printf("enter two no");
+    scanf("%d %d",&a,&b);
+    c=a*b;
+    printf("%d*%d=%d\n",a,b,c);
+}
---
+++ temp.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+float a,b;
+printf("enter temprature in celcius");
+scanf("%f,&a");
+b=a*(9.0/5.0)+32;
+printf("%f=%f\n",a,b);
+
+}
